6b9ec686c78a2046d5146280e042a8be61442746
Abbas Siddiqi <abbas.siddiqi1@gmail.com>
2023-12-05 00:10:43 +0500

    Implement home page with products catalogue

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,26 @@
 <script setup lang="ts">
+import { onMounted } from 'vue';
+import { useProductsStore } from '@/stores/products';
+import { storeToRefs } from 'pinia';
+import ProductItem from '@/components/ProductItem.vue';
+
+const productsStore = useProductsStore();
+const { products } = storeToRefs(productsStore);
+
+onMounted(async () => {
+  await productsStore.fetchProducts();
+});
 
 </script>
 
 <template>
-  <main>
-    <h1 class="text-3xl">Home</h1>
-    <button class="btn">Next</button>
+  <main class="container max-w-5xl mx-auto p-4 pb-8">
+    
+    <input type="text" placeholder="Search products..." class="input input-bordered max-md:my-2 mt-6 md:mb-4 w-full max-w-md" />
+    
+    <div class="grid grid-cols-3 gap-4 max-md:grid-cols-2">
+      <ProductItem v-for="product in products" :key="product.id" :product="product" />
+    </div>
+    
   </main>
 </template>
