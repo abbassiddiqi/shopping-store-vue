ddc363bc7a3c38a6491da2e1dc7fea5138b04a94
Abbas Siddiqi <abbas.siddiqi1@gmail.com>
2023-12-04 23:57:38 +0500

    Implement navbar header component

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavbarHeader from './components/NavbarHeader.vue';
 </script>
 
 <template>
-  <header>
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
-  </header>
+  <NavbarHeader />
 
-  <RouterView />
+  <div>
+    <RouterView />
+  </div>
 </template>
