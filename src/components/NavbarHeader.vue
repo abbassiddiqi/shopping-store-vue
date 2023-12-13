3d66389caee6dbe96d8a961c103dcadc8b43c7dc
Abbas Siddiqi <abbas.siddiqi1@gmail.com>
2023-12-13 15:15:52 +0500

    Fix cart dropdown popover spacing

--- src/components/NavbarHeader.vue
+++ src/components/NavbarHeader.vue
@@ -23,16 +23,16 @@ const { items, total } = storeToRefs(cartStore);
       </div>
       <div class="flex-none flex">
 
-        <div class="dropdown dropdown-end">
+        <div class="dropdown dropdown-end mr-1">
           <Popover>
-            <PopoverButton class="btn btn-ghost btn-circle">
+            <PopoverButton class="btn btn-ghost btn-circle outline-none">
               <div class="indicator">
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
                 <span class="badge badge-sm indicator-item">{{ items.length }}</span>
               </div>
             </PopoverButton>
 
-            <PopoverPanel v-slot="{ close }" class="dropdown-content card card-compact mt-[0.87rem] z-[1] w-52 bg-base-100 shadow">
+            <PopoverPanel v-slot="{ close }" class="dropdown-content card card-compact mt-4 z-[1] w-52 bg-base-100 shadow">
               <div class="card-body">
                 <span class="font-bold text-lg">{{ items.length }} Items</span>
                 <span class="text-info">Subtotal: {{ currencyFormatter.format(total) }}</span>
